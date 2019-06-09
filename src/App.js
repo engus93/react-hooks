3ff1d6c54647fc2581574cd9edd79fe3ce34d991
DuHyeon <engus983@gmail.com>
2019-06-09 11:22:00 +0900

    FEAT: #1.0 Introduction to useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incremetItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen</h2>
+      <button onClick={incremetItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   );
-}
+};
 
 export default App;
